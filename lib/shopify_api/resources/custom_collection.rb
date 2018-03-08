ee677bd743a2a27c6d1c5c3f8047541111eb6a3d
Dave Lazar <hunkybill@gmail.com>
2018-03-08 11:40:32 -0500

    added a sorted_products method to the custom collection

--- lib/shopify_api/resources/custom_collection.rb
+++ lib/shopify_api/resources/custom_collection.rb
@@ -3,6 +3,46 @@ module ShopifyAPI
     include Events
     include Metafields
 
+    # Algorithm to return products in the order driven by the collects
+    # 1. Get all the collects for a collection, store them in a hash, so now we have the 3 key details of sort order, collect_id, product_id and position
+    # 2. Get all the products for a collection, and for each product found, place it in an ordering based on it's position
+    # 3. Return the sorted products for use in the App
+    def sorted_products
+      products = []
+      collects = {}
+      page = 1
+      count = Collect.count(collection_id: self.id)
+      return unless count > 0
+      page += count.divmod(250).first
+      while page > 0
+        list = Collect.all(params: { collection_id: self.id, page: page })
+        collects.merge! list.inject({}) { |result, el| result[el.product_id] = { collect_id: el.id, position: el.position }; result }
+        page -= 1
+      end
+      #puts "\ncollects: #{collects.inspect}\n"
+      page = 1
+      count = Product.count
+      return unless count > 0
+      page += count.divmod(250).first
+      status = {
+        fields: 'id,title,handle,variants,images',
+        limit: 250,
+        collection_id: self.id
+      }
+      while page > 0
+        status[:page] = page
+        list = Product.all(params: status)
+        # Now we do our trickery and add a position to each element so that sorting becomes easy.
+        list.each do |product|
+          product.tap { |p| p.position = collects[product.id][:position]; p.collect_id = collects[product.id][:collect_id] }
+        end
+        products += list
+        page -= 1
+      end
+      products.sort_by(&:position)
+    end
+
+
     def products
       Product.find(:all, :params => {:collection_id => self.id})
     end
